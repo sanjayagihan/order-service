63b4108024cc1dfcbecea17382b12a52e6168b51
sanjayagihan <e18412@eng.pdn.ac.lk>
2024-09-25 02:51:09 +0530

    commiting complete order service

---
+++ src/main/java/com/shop/order_service/dto/OrderRequest.java
@@ -0,0 +1,14 @@
+package com.shop.order_service.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderRequest {
+    private List<OrderLineItemsDto> orderLineItemsDtoList;
+}
